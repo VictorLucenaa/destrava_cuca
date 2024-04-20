c5621ca1e8e56da2d5ff3fd9c86ee4644e826cda
Victor Lucena <victor.lucenaaa@gmail.com>
2024-04-19 21:39:01 -0300

    initial commit

---
+++ src/main/java/com/destravaCuca/DestravaCucaApplication.java
@@ -0,0 +1,13 @@
+package com.destravaCuca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestravaCucaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestravaCucaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/destravaCuca/DestravaCucaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.destravaCuca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestravaCucaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
