d2a8546aafbb874917f57dc4073239d914adaef7
Victor Lucena <victor.lucenaaa@gmail.com>
2024-04-22 22:59:42 -0300

    refact: Removed the StudentDetailDTO and UpdateStudentDTO, now there is only one DTO for Student

--- src/main/java/com/destravaCuca/dto/student/StudentDTO.java
+++ src/main/java/com/destravaCuca/dto/student/StudentDTO.java
@@ -1,26 +1,32 @@
-package com.destravaCuca.dto;
+package com.destravaCuca.dto.student;
 
+import com.destravaCuca.domain.student.Student;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 
 import java.time.LocalDate;
 
-public record CreateStudentDTO(
+public record StudentDTO(
 
-         long id,
-         @NotBlank
          String firstName,
-         @NotBlank
          String middleName,
 
          String gender,
-         @NotNull
          String phone,
 
          @Past
          LocalDate dateOfBirth,
 
-         @NotBlank
          String schoolGrade) {
+
 }
+
+
+
+
+
+
+
+
+
